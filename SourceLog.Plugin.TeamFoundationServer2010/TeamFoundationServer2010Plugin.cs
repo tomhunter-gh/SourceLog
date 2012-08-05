938cc89a8e10cc09c20ef5027b03f2c90d7b0ff7
Tom Hunter <tomhunter@runbox.com>
2012-08-05 15:36:48 +0100

    Adding plugin for Team Foundation Server 2010.

---
+++ SourceLog.Plugin.TeamFoundationServer2010/TeamFoundationServer2010Plugin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SourceLog.Interface;
+using SourceLog.Model;
+
+namespace SourceLog.Plugin.TeamFoundationServer2010
+{
+	public class TeamFoundationServer2010Plugin : ILogProvider<ChangedFile>
+	{
+		public string SettingsXml
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public DateTime MaxDateTimeRetrieved
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public void Initialise()
+		{
+			throw new NotImplementedException();
+		}
+
+		public event NewLogEntryEventHandler<ChangedFile> NewLogEntry;
+	}
+}
