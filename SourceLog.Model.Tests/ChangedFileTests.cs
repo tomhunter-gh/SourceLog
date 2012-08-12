f9d06354a7a6141332e6500af03f2db363b971a1
Tom Hunter <tomhunter@runbox.com>
2012-08-12 01:04:13 +0100

    Adding unit test project Adding new unit test to check the size of the serialized FlowDocuments for a specific change and ensure they are under a specific threshold. Adding reference to Moq to mock the SourceLogContext.

---
+++ SourceLog.Model.Tests/ChangedFileTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SourceLog.Interface;
+
+namespace SourceLog.Model.Tests
+{
+	[TestClass]
+	public class ChangedFileTests
+	{
+		[TestMethod]
+		[DeploymentItem(@"TestData\jquery.signalR.core.js.oldversion")]
+		[DeploymentItem(@"TestData\jquery.signalR.core.js.newversion")]
+		public void OldNewVersionTextStorageSizeThreshold()
+		{
+			var logSubscription = new LogSubscription
+				{
+					LogSubscriptionId = 1,
+					Log = new ObservableCollection<LogEntry>()
+				};
+
+			var fakeLogSubscriptionDbSet = new FakeDbSet<LogSubscription> { logSubscription };
+
+			var mockContext = new Mock<ISourceLogContext>();
+			mockContext.Setup(m => m.LogSubscriptions).Returns(fakeLogSubscriptionDbSet);
+
+			SourceLogContext.ThreadStaticContext = mockContext.Object;
+
+			var changedFile = new ChangedFile();
+
+			using (var reader = new StreamReader("jquery.signalR.core.js.oldversion"))
+			{
+				changedFile.OldVersion = reader.ReadToEnd();
+			}
+
+			using (var reader = new StreamReader("jquery.signalR.core.js.newversion"))
+			{
+				changedFile.NewVersion = reader.ReadToEnd();
+			}
+
+			var logEntry = new LogEntry
+			{
+				ChangedFiles = new List<ChangedFile> { changedFile }
+			};
+
+			logSubscription.AddNewLogEntry(this, new NewLogEntryEventArgs<ChangedFile> { LogEntry = logEntry });
+
+
+			Assert.IsTrue(changedFile.LeftFlowDocumentData.Length <= 6716,
+				"changedFile.LeftFlowDocumentData.Length: " + changedFile.LeftFlowDocumentData.Length);
+
+			Assert.IsTrue(changedFile.RightFlowDocumentData.Length <= 6708,
+				"changedFile.RightFlowDocumentData.Length: " + changedFile.RightFlowDocumentData.Length);
+		}
+	}
+}
---
+++ SourceLog.Model.Tests/FakeDbSet.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+
+namespace SourceLog.Model.Tests
+{
+	public class FakeDbSet<T> : IDbSet<T> where T : class
+	{
+		readonly HashSet<T> _data;
+		readonly IQueryable _query;
+
+		public FakeDbSet()
+		{
+			_data = new HashSet<T>();
+			_query = _data.AsQueryable();
+		}
+
+		public virtual T Find(params object[] keyValues)
+		{
+			throw new NotImplementedException("Derive from FakeDbSet<T> and override Find");
+		}
+
+		T IDbSet<T>.Add(T entity)
+		{
+			_data.Add(entity);
+			return entity;
+		}
+
+		public void Add(T entity)
+		{
+			_data.Add(entity);
+		}
+
+		T IDbSet<T>.Remove(T entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		T IDbSet<T>.Attach(T entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public T Create()
+		{
+			throw new NotImplementedException();
+		}
+
+		public TDerivedEntity Create<TDerivedEntity>() where TDerivedEntity : class, T
+		{
+			throw new NotImplementedException();
+		}
+
+		public ObservableCollection<T> Local { get; set; }
+
+		Type IQueryable.ElementType
+		{
+			get { return _query.ElementType; }
+		}
+		System.Linq.Expressions.Expression IQueryable.Expression
+		{
+			get { return _query.Expression; }
+		}
+		IQueryProvider IQueryable.Provider
+		{
+			get { return _query.Provider; }
+		}
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			return _data.GetEnumerator();
+		}
+		IEnumerator<T> IEnumerable<T>.GetEnumerator()
+		{
+			return _data.GetEnumerator();
+		}
+	}
+}
