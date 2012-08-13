7c0cab2ae906f2a0f1d4231a69b458d71e622025
Tom Hunter <tomhunter@runbox.com>
2012-08-13 21:28:54 +0100

    Display number of unread log entries
    Making the log subscription name appear bold if there are unread log
    entries, and the number of unread entries appear in brackets after the
    name.

---
+++ SourceLog.UI/SubscriptionLabelConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using SourceLog.Model;
+
+namespace SourceLog
+{
+	public class SubscriptionLabelConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			var name = (string)values[0];
+			var unreadCount = ((ObservableCollection<LogEntry>) values[1]).Count(le => !le.Read);
+
+			return name + (unreadCount > 0 ? " (" + unreadCount + ")" : String.Empty);
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
