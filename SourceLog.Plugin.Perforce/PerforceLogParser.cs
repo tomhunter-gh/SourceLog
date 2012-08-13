782121dd9eb6286ce780c0ac9eb1073f5d76e1d3
Tom Hunter <tomhunter@runbox.com>
2012-08-13 19:20:46 +0100

    Started Perforce plugin implementation
    Using P4COM for now as server version is 2006.1/109255.

---
+++ SourceLog.Plugin.Perforce/PerforceLogParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SourceLog.Model;
+using System.Text.RegularExpressions;
+
+namespace SourceLog.Plugin.Perforce
+{
+	public static class PerforceLogParser
+	{
+		internal static LogEntry Parse(string changesetString)
+		{
+			var logEntry = new LogEntry();
+
+			string pattern = @"Change\s(?<revision>\d+)\son\s(?<datetime>\d{4}/\d{2}/\d{2}\s\d{2}:\d{2}:\d{2})\sby\s(?<author>\w+)@\w+\n\n\t(?<message>.*)";
+			Regex r = new Regex(pattern);
+			Match match = r.Match(changesetString);
+			if (match.Success)
+			{
+				int revision = 0;
+				if (Int32.TryParse(match.Groups["revision"].Value, out revision))
+					logEntry.Revision = revision.ToString();
+
+				DateTime datetime = DateTime.MinValue;
+				if (DateTime.TryParse(match.Groups["datetime"].Value, out datetime))
+					logEntry.CommittedDate = datetime;
+
+				logEntry.Author = match.Groups["author"].Value;
+				logEntry.Message = match.Groups["message"].Value;
+
+			}
+
+			return logEntry;
+		}
+
+		internal static ChangedFile ParseP4File(string file)
+		{
+			var changedFile = new ChangedFile();
+			string pattern = @"(?<filename>[^#]*)#(?<revision>\d+)\s-\s(?<action>\w)\schange\s(?<changeNumber>\d+)\s\((?<filetype>\w+)\)";
+			Regex r = new Regex(pattern);
+			Match match = r.Match(file);
+			if (match.Success)
+			{
+				changedFile.FileName = match.Groups["filename"].Value;
+			}
+
+			return changedFile;
+		}
+	}
+}
---
+++ SourceLog.Plugin.Perforce/PerforcePlugin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SourceLog.Interface;
+using SourceLog.Model;
+using System.Threading;
+
+namespace SourceLog.Plugin.Perforce
+{
+	public class PerforcePlugin : ILogProvider<ChangedFile>
+	{
+		private Timer _timer;
+		private readonly Object _lockObject = new Object();
+
+		public string SettingsXml { get; set; }
+
+		public DateTime MaxDateTimeRetrieved { get; set; }
+
+		public void Initialise()
+		{
+			_timer = new Timer(CheckForNewLogEntries);
+			_timer.Change(0, 15000);
+		}
+
+		private void CheckForNewLogEntries(object state)
+		{
+			if (Monitor.TryEnter(_lockObject))
+			{
+				try
+				{
+					List<LogEntry> logEntries = new List<LogEntry>();
+					P4COM.p4 p4 = new P4COM.p4();
+					p4.Connect();
+					var p4changes = p4.run("changes -t -l -s submitted -m 30 \"//depot/trunk/...\"");
+					foreach (string changeset in p4changes)
+					{
+						var logEntry = PerforceLogParser.Parse(changeset);
+						if (logEntry.CommittedDate <= MaxDateTimeRetrieved)
+							continue;
+						logEntries.Add(logEntry);
+					}
+
+					foreach (var logEntry in logEntries.OrderBy(le => le.CommittedDate))
+					{
+						// grab changed files
+						var p4files = p4.run("files @=" + logEntry.Revision);
+						foreach (string file in p4files)
+						{
+							ChangedFile changedFile = PerforceLogParser.ParseP4File(file);
+						}
+
+						// send to Model
+					}
+				}
+				finally
+				{
+					Monitor.Exit(_lockObject);
+				}
+			}
+		}
+
+		public event NewLogEntryEventHandler<ChangedFile> NewLogEntry;		
+	}
+}
