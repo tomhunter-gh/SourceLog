5afd74f858deb9b744a543aaa70a076928cf7d0b
Tom Hunter <tomhunter@runbox.com>
2012-11-27 23:20:55 +0000

    Updating AssemblyVersion

---
+++ SourceLog.UI/Properties/GlobalAssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SourceLog")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2012.11.27.2318")]
+[assembly: AssemblyFileVersion("2012.11.27.2318")]
