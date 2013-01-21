674961d6c7a67ba6ba21e4829308ed808f7299c9
Tom Hunter <tomhunter@runbox.com>
2013-01-21 22:32:13 +0000

    Mark as read command
    Adding keyboard shortcut to mark current item as read, and subscription
    context menu option to mark all subscription log entries as read.

---
+++ SourceLog.UI/MarkAsReadCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+using SourceLog.Model;
+
+namespace SourceLog
+{
+	public class MarkAsReadCommand : ICommand
+	{
+		public void Execute(object parameter)
+		{
+			var readItem = parameter as LogEntry;
+			if (readItem != null)
+				if (!readItem.Read)
+				{
+					readItem.Read = true;
+					readItem.MarkAsReadAndSave();
+				}
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return (parameter as LogEntry) != null;
+		}
+
+		public event EventHandler CanExecuteChanged;
+	}
+}
