460d93a3fc55da78db56fa775ff64862229a631a
Tom Hunter <tomhunter@runbox.com>
2013-01-22 21:59:54 +0000

    Register p4com once only
    Use a static constructor to register p4com once only.
    Using EditorConfig to ensure VS uses tabs for this project -
    http://editorconfig.org

--- SourceLog.Plugin.Perforce/PerforcePlugin.cs
+++ SourceLog.Plugin.Perforce/PerforcePlugin.cs
@@ -32,103 +32,97 @@ namespace SourceLog.Plugin.Perforce
 				Logger.Write(new LogEntry { Message = "Checking for new entries", Categories = { "Plugin.Perforce" } });
 				try
 				{
-                    using (Registrar registrar = new Registrar(
-                        Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\p4com.dll"))
-                    {
-                        registrar.RegisterComDLL();
+					var p4 = new p4();
+					p4.Connect();
+					var repoPath = SettingsXml;
+					if (repoPath.EndsWith(@"/"))
+						repoPath += "...";
+					var p4Changes = p4.run("changes -t -l -s submitted -m 30 \"" + repoPath + "\"");
 
-                        var p4 = new p4();
-                        p4.Connect();
-                        var repoPath = SettingsXml;
-                        if (repoPath.EndsWith(@"/"))
-                            repoPath += "...";
-                        var p4Changes = p4.run("changes -t -l -s submitted -m 30 \"" + repoPath + "\"");
+					var logEntries = p4Changes.Cast<string>().Select(PerforceLogParser.Parse).Where(logEntry => logEntry.CommittedDate > MaxDateTimeRetrieved).ToList();
 
-                        var logEntries = p4Changes.Cast<string>().Select(PerforceLogParser.Parse).Where(logEntry => logEntry.CommittedDate > MaxDateTimeRetrieved).ToList();
+					foreach (var logEntry in logEntries.OrderBy(le => le.CommittedDate))
+					{
+						logEntry.ChangedFiles = new List<ChangedFileDto>();
 
-                        foreach (var logEntry in logEntries.OrderBy(le => le.CommittedDate))
-                        {
-                            logEntry.ChangedFiles = new List<ChangedFileDto>();
+						// grab changed files
+						var p4Files = p4.run("files @=" + logEntry.Revision);
+						foreach (string file in p4Files)
+						{
+							ChangedFileDto changedFile = PerforceLogParser.ParseP4File(file);
+							if (changedFile.ChangeType == ChangeType.Added
+								|| changedFile.ChangeType == ChangeType.Copied
+								|| changedFile.ChangeType == ChangeType.Modified
+								|| changedFile.ChangeType == ChangeType.Moved)
+							{
+								if (changedFile.ChangeType == ChangeType.Copied
+									|| changedFile.ChangeType == ChangeType.Moved)
+								{
+									// Add new path to top of NewVersion
+									changedFile.NewVersion = String.Empty;
+								}
+								else
+								{
+									changedFile.NewVersion = String.Empty;
+								}
 
-                            // grab changed files
-                            var p4Files = p4.run("files @=" + logEntry.Revision);
-                            foreach (string file in p4Files)
-                            {
-                                ChangedFileDto changedFile = PerforceLogParser.ParseP4File(file);
-                                if (changedFile.ChangeType == ChangeType.Added
-                                    || changedFile.ChangeType == ChangeType.Copied
-                                    || changedFile.ChangeType == ChangeType.Modified
-                                    || changedFile.ChangeType == ChangeType.Moved)
-                                {
-                                    if (changedFile.ChangeType == ChangeType.Copied
-                                        || changedFile.ChangeType == ChangeType.Moved)
-                                    {
-                                        // Add new path to top of NewVersion
-                                        changedFile.NewVersion = String.Empty;
-                                    }
-                                    else
-                                    {
-                                        changedFile.NewVersion = String.Empty;
-                                    }
+								// Always create a new p4 object otherwise TempFilename doesn't always update
+								var p4Print = new p4();
+								p4Print.Connect();
+								p4Print.run("print \"" + changedFile.FileName + "@" + logEntry.Revision + "\"");
+								string tempFilename = ((dynamic)p4Print).TempFilename;
 
-                                    // Always create a new p4 object otherwise TempFilename doesn't always update
-                                    var p4Print = new p4();
-                                    p4Print.Connect();
-                                    p4Print.run("print \"" + changedFile.FileName + "@" + logEntry.Revision + "\"");
-                                    string tempFilename = ((dynamic)p4Print).TempFilename;
+								using (var streamReader = new StreamReader(tempFilename))
+								{
+									changedFile.NewVersion += streamReader.ReadToEnd();
+								}
+								p4Print.Disconnect();
+								File.SetAttributes(tempFilename, FileAttributes.Normal);
+								File.Delete(tempFilename);
+							}
+							else
+							{
+								changedFile.NewVersion = String.Empty;
+							}
 
-                                    using (var streamReader = new StreamReader(tempFilename))
-                                    {
-                                        changedFile.NewVersion += streamReader.ReadToEnd();
-                                    }
-                                    p4Print.Disconnect();
-                                    File.SetAttributes(tempFilename, FileAttributes.Normal);
-                                    File.Delete(tempFilename);
-                                }
-                                else
-                                {
-                                    changedFile.NewVersion = String.Empty;
-                                }
+							if (changedFile.ChangeType == ChangeType.Deleted
+								|| changedFile.ChangeType == ChangeType.Modified)
+							{
+								var p4Print = new p4();
+								p4Print.Connect();
+								p4Print.run("print \"" + changedFile.FileName + "@" + (Int32.Parse(logEntry.Revision) - 1) + "\"");
+								string tempFilename = ((dynamic)p4Print).TempFilename;
+								using (var streamReader = new StreamReader(tempFilename))
+								{
+									changedFile.OldVersion = streamReader.ReadToEnd();
+								}
+								p4Print.Disconnect();
+								File.SetAttributes(tempFilename, FileAttributes.Normal);
+								File.Delete(tempFilename);
+							}
+							else if (changedFile.ChangeType == ChangeType.Copied
+								|| changedFile.ChangeType == ChangeType.Moved)
+							{
+								// TODO: get previous path and contents and put both in OldVersion
+								changedFile.OldVersion = String.Empty;
+							}
+							else
+							{
+								changedFile.OldVersion = String.Empty;
+							}
 
-                                if (changedFile.ChangeType == ChangeType.Deleted
-                                    || changedFile.ChangeType == ChangeType.Modified)
-                                {
-                                    var p4Print = new p4();
-                                    p4Print.Connect();
-                                    p4Print.run("print \"" + changedFile.FileName + "@" + (Int32.Parse(logEntry.Revision) - 1) + "\"");
-                                    string tempFilename = ((dynamic)p4Print).TempFilename;
-                                    using (var streamReader = new StreamReader(tempFilename))
-                                    {
-                                        changedFile.OldVersion = streamReader.ReadToEnd();
-                                    }
-                                    p4Print.Disconnect();
-                                    File.SetAttributes(tempFilename, FileAttributes.Normal);
-                                    File.Delete(tempFilename);
-                                }
-                                else if (changedFile.ChangeType == ChangeType.Copied
-                                    || changedFile.ChangeType == ChangeType.Moved)
-                                {
-                                    // TODO: get previous path and contents and put both in OldVersion
-                                    changedFile.OldVersion = String.Empty;
-                                }
-                                else
-                                {
-                                    changedFile.OldVersion = String.Empty;
-                                }
+							logEntry.ChangedFiles.Add(changedFile);
+						}
 
-                                logEntry.ChangedFiles.Add(changedFile);
-                            }
+						var args = new NewLogEntryEventArgs { LogEntry = logEntry };
+						NewLogEntry(this, args);
+					}
 
-                            var args = new NewLogEntryEventArgs { LogEntry = logEntry };
-                            NewLogEntry(this, args);
-                        }
-
-                        p4.Disconnect();
-                        if (logEntries.Count > 0)
-                        {
-                            MaxDateTimeRetrieved = logEntries.Max(x => x.CommittedDate);
-                        }
-                    }
+					p4.Disconnect();
+					if (logEntries.Count > 0)
+					{
+						MaxDateTimeRetrieved = logEntries.Max(x => x.CommittedDate);
+					}
 				}
 				catch (Exception ex)
 				{
@@ -144,5 +138,31 @@ namespace SourceLog.Plugin.Perforce
 
 		public event NewLogEntryEventHandler NewLogEntry;
 		public event LogProviderExceptionEventHandler LogProviderException;
+
+		// Manage registration of p4com
+		// using static constructor to ensure registration happens only once
+		static PerforcePlugin()
+		{
+			Registrar registrar = new Registrar(
+						Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\p4com.dll");
+			registrar.RegisterComDLL();
+			finalizer = new Finalizer(registrar);
+		}
+
+		static readonly Finalizer finalizer;
+
+		sealed class Finalizer
+		{
+			private Registrar _registrar;
+			internal Finalizer(Registrar registrar)
+			{
+				_registrar = registrar;
+			}
+
+			~Finalizer()
+			{
+				_registrar.Dispose();
+			}
+		}
 	}
 }
