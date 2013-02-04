a3466927bae51ed625d01dc3473687bede3a0e0c
Tom Hunter <tomhunter@runbox.com>
2013-02-04 23:45:17 +0000

    Adding msvcr100.dll
    Seems to be required by LibGit2 and isn't installed on a fresh WinXP nor
    by the .NET Framework 4 installer.
    See https://github.com/libgit2/libgit2/issues/255#issuecomment-13105857

---
+++ AppHarborBuildToTemp.ps1
@@ -0,0 +1,4 @@
+$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+$outdir = "C:\temp\SourceLog\SourceLog" + (get-date -f yyyyMMddHHmmss) + "\"
+$cmd = $msbuild + " SourceLog.sln /property:Configuration=Release /property:OutDir=" + $outdir
+Invoke-Expression $cmd
