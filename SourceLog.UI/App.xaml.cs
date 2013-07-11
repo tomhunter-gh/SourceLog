909f7d1bccf1cbf19322ee9d5fca8fce5eb0753f
Tom Hunter <tomhunter@runbox.com>
2013-07-11 15:48:37 +0100

    Limit app to single instance
    Using approach outlined here:
    http://blogs.microsoft.co.il/blogs/arik/archive/2010/05/28/wpf-single-instance-application.aspx
    
    Fixes #21

--- SourceLog.UI/App.xaml.cs
+++ SourceLog.UI/App.xaml.cs
@@ -1,16 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
 using System.Windows;
 using Microsoft.Practices.EnterpriseLibrary.Logging;
+using Microsoft.Shell;
 
 namespace SourceLog
 {
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
-	public partial class App
+	public partial class App : ISingleInstanceApp
 	{
+		private const string Unique = "SourceLog";
+
+		/// <summary>
+		/// http://blogs.microsoft.co.il/blogs/arik/archive/2010/05/28/wpf-single-instance-application.aspx
+		/// </summary>
+		[STAThread]
+		public static void Main()
+		{
+			if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+			{
+				var application = new App();
+
+				application.InitializeComponent();
+				application.Run();
+
+				// Allow single instance code to perform cleanup operations
+				SingleInstance<App>.Cleanup();
+			}
+		}
+
 		public App()
 		{
 			AppDomain.CurrentDomain.UnhandledException += CurrentDomainUnhandledException;
@@ -35,6 +57,12 @@ namespace SourceLog
 					Message = "Application closing..",
 					Severity = TraceEventType.Information
 				});
+		}
+
+		public bool SignalExternalCommandLineArgs(IList<string> args)
+		{
+			MainWindow.Activate();
+			return true;
 		}
 	}
 }
